ac60178f273bb571d4c691232f4822b9ed46d88f
MAL1S <karbushev_ivan@mail.ru>
2023-03-03 19:44:52 +0800

    Added requests and responses

--- src/main/kotlin/data/remote/api/AdminProjectFairApi.kt
+++ src/main/kotlin/data/remote/api/AdminProjectFairApi.kt
@@ -5,12 +5,10 @@ import data.dto.ProjectResponse
 import retrofit2.http.GET
 
 interface AdminProjectFairApi {
-    //make get only active ones
+
     @GET("projects")
     suspend fun getProjects(): List<ProjectResponse>
 
     @GET("participations")
     suspend fun getParticipations(): List<ParticipationResponse>
-
-    //get students
 }
--- src/main/kotlin/data/remote/api/OrdinaryProjectFairApi.kt
+++ src/main/kotlin/data/remote/api/OrdinaryProjectFairApi.kt
@@ -1,9 +1,13 @@
 package data.remote.api
 
 import data.dto.InstituteResponse
+import data.dto.StudentResponse
 import retrofit2.http.GET
 
 interface OrdinaryProjectFairApi {
     @GET("institutes")
     suspend fun getInstitutes(): List<InstituteResponse>
+
+    @GET("candidates")
+    suspend fun getCandidates(): List<StudentResponse>
 }
